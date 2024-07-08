71177b3ec255d1568e93d093e0dbfaa60c8432bb
Trần Anh Đức <ducanhtran28112x@gmail.com>
2024-07-08 12:54:00 +0700

    update code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,76 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {ServerService} from "./service/server.service";
+import {BehaviorSubject, catchError, map, Observable, of, startWith} from "rxjs";
+import {AppState} from "./interface/app-state";
+import {CustomResponse} from "./interface/custom-response";
+import {DataState} from "./enum/data-state";
+import {Status} from "./enum/status";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
-  title = 'app-state-management-frontend';
+export class AppComponent implements OnInit {
+  appState$?: Observable<AppState<CustomResponse>>;
+  readonly DataState = DataState;
+  readonly Status = Status;
+
+  // biến lưu giá trị để hiển thị vòng xoay hoặc bộ định tuyến
+  private filterSubject = new BehaviorSubject<string>('');
+  private dataSubject = new BehaviorSubject<CustomResponse | undefined>(undefined);
+  filterStatus$ = this.filterSubject.asObservable();
+
+  constructor(private serverService: ServerService) {
+  }
+
+  ngOnInit(): void {
+    this.appState$ = this.serverService.servers$
+      .pipe( // Kết nối nhiều toán tử để xử lý và biến đổi luồng dữ liệu.
+        // Biến đổi dữ liệu từ API thành cấu trúc dữ liệu phù hợp với trạng thái của ứng dụng.
+        map((response: CustomResponse) => {
+          this.dataSubject.next(response); // lưu lại giá trị trả về
+          return {dataState: DataState.LOADED_STATE, appData: response}
+        }),
+        // Đặt một giá trị ban đầu cho Observable ngay lập tức khi nó được subscribe
+        startWith({dataState: DataState.LOADING_STATE}),
+        catchError((error: string) => {
+          return of({dataState: DataState.ERROR_STATE, error})
+        }),
+      )
+  }
+
+
+  pingServer(ipAddress: string): void {
+    // lưu vào biến filterSubject để gọi filterStatus$: Observable<string> và để thay đổi UI cột ping theo logic
+    this.filterSubject.next(ipAddress);
+    this.appState$ = this.serverService.ping$(ipAddress)
+      .pipe(
+        map((response: CustomResponse) => { // trả về 1 server muốn kết nối
+          const index = this.dataSubject.value?.data.servers?.findIndex(server => server.id === response.data.server?.id);
+          if (index && index !== -1) {
+            if (this.dataSubject.value &&
+              this.dataSubject.value.data &&
+              this.dataSubject.value.data.servers &&
+              response.data.server) {
+              this.dataSubject.value.data.servers[index] = response.data.server;
+            }
+          }
+          // đặt chuỗi về rỗng để ngừng hiển thị vòng quay và hiển thị bộ định tuyến người dùng có thể ping lại
+          this.filterSubject.next('');
+          return {dataState: DataState.LOADED_STATE, appData: this.dataSubject.value}; // trả về data mới nhất
+        }),
+        // Đặt giá trị ban đầu cho Observable này khi subscribe là list dữ liệu server đã tải nên trước đó
+        startWith({dataState: DataState.LOADING_STATE, appData: this.dataSubject.value}),
+        catchError((error: string) => {
+          // ngừng hiển thị vòng quay ngay cả khi lỗi
+          this.filterSubject.next('');
+          return of({dataState: DataState.ERROR_STATE, error})
+        }),
+      )
+  }
+
+
 }
---
+++ src/app/service/server.service.ts
@@ -0,0 +1,112 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {catchError, Observable, tap, throwError} from "rxjs";
+import {CustomResponse} from "../interface/custom-response";
+import {Server} from "../interface/server";
+import {Status} from "../enum/status";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServerService {
+
+  private readonly apiUrl = 'http://localhost:8080/api/server';
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  // C1: viết kiểu method
+  // getServers(): Observable<CustomResponse> {
+  //   return this.httpClient.get<CustomResponse>(`${this.apiUrl}/list`)
+  //     .pipe(
+  //       tap(console.log),
+  //       catchError(this.handleError)
+  //     );
+  // }
+
+  // servers$: biến này là một Observable
+  servers$ = <Observable<CustomResponse>>
+    this.httpClient.get<CustomResponse>(`${this.apiUrl}/list`)
+      .pipe(
+        tap(console.log), // tap: Xử lý side effect - ghi log dữ liệu phản hồi từ API vào console, mà không làm thay đổi dữ liệu.
+        catchError(this.handleError)  // catchError: Bắt và xử lý lỗi nếu yêu cầu HTTP thất bại.
+      );
+
+  save$ = (server: Server) => <Observable<CustomResponse>>
+    this.httpClient.post<CustomResponse>(`${this.apiUrl}/save`, server)
+      .pipe(
+        tap(console.log),
+        catchError(this.handleError)
+      );
+
+  ping$ = (ipAddress: String) => <Observable<CustomResponse>>
+    this.httpClient.get<CustomResponse>(`${this.apiUrl}/ping/${ipAddress}`)
+      .pipe(
+        tap(console.log),
+        catchError(this.handleError)
+      );
+
+  filter$ = (status: Status, response: CustomResponse) => <Observable<CustomResponse>>
+    new Observable<CustomResponse>(
+      subscriber => {
+        console.log(response);
+
+        // let updatedResponse;
+        // // nếu chọn Server status ALL trả về tất cả Server
+        // if (status === Status.ALL) {
+        //   updatedResponse = {
+        //     ...response,
+        //     message: `Servers filtered by ${status} status`
+        //   };
+        // } else { // nếu chọn Server status khác ALL, trả về danh sách Server theo status
+        //   const filteredServers = response.data.servers?.filter(server => server.status === status);
+        //   updatedResponse = {
+        //     ...response,
+        //     message: (filteredServers && filteredServers.length > 0) ?
+        //       `Servers filtered by ${status === Status.SERVER_UP ? 'SERVER UP' : 'SERVER DOWN'} status` :
+        //       `No servers of ${status} found`,
+        //     data: {servers: filteredServers}
+        //   }
+        // }
+
+        const filteredServers = response.data.servers?.filter(server => server.status === status);
+        let updatedResponse =
+          status === Status.ALL ?
+            {
+              ...response,
+              message: `Servers filtered by ${status} status`} :
+            {
+              ...response,
+              message: (filteredServers && filteredServers.length > 0) ?
+                `Servers filtered by ${status === Status.SERVER_UP ? 'SERVER UP' : 'SERVER DOWN'} status` :
+                `No servers of ${status} found`,
+              data: {
+                servers: response.data.servers?.filter(server => server.status === status)
+              }
+            }
+        subscriber.next(updatedResponse)
+        subscriber.complete();
+      }
+    ).pipe(
+      tap(console.log),
+      catchError(this.handleError)
+    );
+
+  delete$ = (serverId: number) => <Observable<CustomResponse>>
+    this.httpClient.delete<CustomResponse>(`${this.apiUrl}/delete/${serverId}`)
+      .pipe(
+        tap(console.log),
+        catchError(this.handleError)
+      );
+
+
+  /*
+   * Observable<never>: không bao giờ phát ra giá trị (next),
+   * không bao giờ hoàn thành (complete),
+   * và không bao giờ gặp lỗi (error).
+   */
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    console.log(error);
+    return throwError(() => new Error(`An error occurred - Error code: ${error.status}`));
+  }
+}
